86d6b5f75bbe07701fa6121d56dc172d5ef15bce
Ataol <ataolbalkar97@gmail.com>
2021-03-10 19:31:59 +0300

    Created 'Question Detail' Page

--- questions/urls.py
+++ questions/urls.py
@@ -4,4 +4,5 @@ from questions import views
 urlpatterns = [
     url(r'^$', views.QuestionsListView.as_view(), name='question_list'),
     url(r'^ask/$', views.AskQuestionView.as_view(), name='ask'),
+    url(r'^question/(?P<pk>\d+)$', views.QuestionDetailView.as_view(), name='question_detail'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from questions.models import Question
 from django.utils import timezone
-from django.views.generic import ListView, DetailView, CreateView, TemplateView
+from django.views.generic import ListView, DetailView, CreateView, TemplateView, FormView
 
+from django.contrib.auth import get_user_model
 
 from questions.forms import QuestionForm
 
 
 # Create your views here.
 
+User = get_user_model()
 
 def baseView(request):
     return render(request, 'questions/base_without_sidebar.html')
@@ -26,6 +28,7 @@ class AskQuestionView(CreateView):
     template_name = 'questions/ask.html'
     form_class = QuestionForm
 
+
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.author = self.request.user
@@ -34,19 +37,29 @@ class AskQuestionView(CreateView):
         return super().form_valid(form)
 
 
-
-
-
-# def ask_question(request):
-#     if request.method == 'POST':
-#         pass
-#
-#     else:
-#         form = QuestionForm
-#
-#
-#         return render(request, 'questions/ask.html', {'form': form})
-
-
 class QuestionDetailView(DetailView):
     model = Question
+    template_name = 'questions/question_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetailView, self).get_context_data(**kwargs)
+        context['modified_author'] = self.object.modified_author
+        context['author'] = self.object.author
+        return context
+
+    def get_object(self, queryset=None):
+        object = super().get_object()
+        object.views += 1
+        object.save()
+
+        return object
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(QuestionDetailView, self).get_context_data(**kwargs)
+    #     context['form'] = AnswerForm
+    #     return context
+
+
+# class AnswerQuestionView(FormView):
+#     form_class = AnswerForm
+#     success_url = ''
