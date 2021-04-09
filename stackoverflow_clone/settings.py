1d98ba46c39083187e79ac81803201c256a213c2
galyus <xgalyusx@gmail.com>
2021-04-09 14:56:14 +0000

    Reset

--- stackoverflow_clone/settings.py
+++ stackoverflow_clone/settings.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'questions/static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/account/images')
 MEDIA_URL = '/images/'
 
