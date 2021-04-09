81cd9a9bb18c1a254d3c08bde0f0e0b72d784947
galyus <xgalyusx@gmail.com>
2021-04-09 14:59:22 +0000

    reset+1

--- stackoverflow_clone/settings.py
+++ stackoverflow_clone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!ydso=%8to#n4mjcy4115(u8tbg0cth5=_zu&(=fz)(krp*e29'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'localhost:8000']
+ALLOWED_HOSTS = ['127.0.0.1', 'pure-island-62704.herokuapp.com']
 
 
 # Application definition
